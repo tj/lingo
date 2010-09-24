9be4310f5c570648558c8837a3b0dc71f955218e
Tj Holowaychuk <tj@vision-media.ca>
2010-09-24 07:57:50 -0700

    Added lingo.uncountable()

--- test/lingo.test.js
+++ test/lingo.test.js
@@ -3,10 +3,19 @@
  * Module dependencies.
  */
 
-var chrono = require('chrono');
+var lingo = require('lingo');
 
 module.exports = {
   'test .version': function(assert){
-    assert.match(chrono.version, /^\d+\.\d+\.\d+$/);
+    assert.match(lingo.version, /^\d+\.\d+\.\d+$/);
+  },
+  
+  'test .uncountables': function(assert){
+    assert.ok(lingo.uncountables);
+  },
+  
+  'test .uncountable()': function(assert){
+    assert.equal(true, lingo.uncountable('moose'));
+    assert.equal(false, lingo.uncountable('person'));
   }
 }

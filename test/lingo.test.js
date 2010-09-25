141dd69dc0979f922120b7e973b4f2e13e94a89f
Tj Holowaychuk <tj@vision-media.ca>
2010-09-25 14:30:54 -0700

    Added lingo.capitalize() and lingo.camelcase()

--- test/lingo.test.js
+++ test/lingo.test.js
@@ -8,5 +8,18 @@ var lingo = require('lingo');
 module.exports = {
   'test .version': function(assert){
     assert.match(lingo.version, /^\d+\.\d+\.\d+$/);
+  },
+  
+  'test .capitalize()': function(assert){
+    assert.equal('Hello there', lingo.capitalize('hello there'));
+    assert.equal('Hello There', lingo.capitalize('hello there', true));
+  },
+  
+  'test .camelcase()': function(assert){
+    assert.equal('foo', lingo.camelcase('foo'));
+    assert.equal('fooBar', lingo.camelcase('foo bar'));
+    assert.equal('fooBarBaz', lingo.camelcase('foo bar baz'));
+    assert.equal('base64Encode', lingo.camelcase('base  -! 64 encode'));
+    assert.equal('UserRole', lingo.camelcase('user role', true));
   }
 }

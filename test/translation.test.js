c6085b658617b1a7dd783b90e9098d1c3d223dfb
Tj Holowaychuk <tj@vision-media.ca>
2010-09-27 09:41:17 -0700

    More translate() assertions

--- test/translation.test.js
+++ test/translation.test.js
@@ -10,5 +10,6 @@ module.exports = {
   'test .translate()': function(assert){
     assert.equal('Hello World', t('Hello World'));
     assert.equal('Hello TJ', t('Hello {name}', { name: 'TJ' }));
+    assert.equal('Hello foo bar', t('Hello {first} {last}', { first: 'foo', last: 'bar' }));
   }
 }

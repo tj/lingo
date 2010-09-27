c79943644f0195a8366fdb1343136cc734eabe04
Tj Holowaychuk <tj@vision-media.ca>
2010-09-27 10:57:17 -0700

    Real examples in test

--- test/translation.test.js
+++ test/translation.test.js
@@ -4,27 +4,26 @@
  */
 
 var lingo = require('lingo')
-  , lang = lingo.Language.en;
+  , en = lingo.Language.en;
 
-var alien = new lingo.Language('alien');
+var fr = new lingo.Language('fr');
 
-alien.translations = {
-    'Hello World': 'woof rawr'
-  , 'Hello {name}': '{name} rawr'
-  , 'Hello {first} {last}': '{first} {last} rawr'
+fr.translations = {
+    'Hello World': 'Bonjour tout le monde'
+  , 'Hello {name}': 'Bonjour {name}'
+  , 'Hello {first} {last}': 'Bonjour {first} {last}'
 };
 
 module.exports = {
   'test .translate()': function(assert){
-    assert.equal('Hello World', lang.translate('Hello World'));
-    assert.equal('Hello TJ', lang.translate('Hello {name}', { name: 'TJ' }));
-    assert.equal('Hello foo bar', lang.translate('Hello {first} {last}', { first: 'foo', last: 'bar' }));
+    assert.equal('Hello World', en.translate('Hello World'));
+    assert.equal('Hello TJ', en.translate('Hello {name}', { name: 'TJ' }));
+    assert.equal('Hello foo bar', en.translate('Hello {first} {last}', { first: 'foo', last: 'bar' }));
   },
   
   'test .translate() with translations': function(assert){
-    var lang = alien;
-    assert.equal('woof rawr', lang.translate('Hello World'));
-    assert.equal('TJ rawr', lang.translate('Hello {name}', { name: 'TJ' }));
-    assert.equal('foo bar rawr', lang.translate('Hello {first} {last}', { first: 'foo', last: 'bar' }));
+    assert.equal('Bonjour tout le monde', fr.translate('Hello World'));
+    assert.equal('Bonjour TJ', fr.translate('Hello {name}', { name: 'TJ' }));
+    assert.equal('Bonjour foo bar', fr.translate('Hello {first} {last}', { first: 'foo', last: 'bar' }));
   }
 }
